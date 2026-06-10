732304039e6e096ea07fe8136f9b7c250c8a9a40
nazrul23-human <nazrul23-human@gmail.com>
2026-06-11 00:00:31 +0600

    Setup Firebase authentication

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,16 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 
+import AuthProvider from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster position="top-center" />
+    </AuthProvider>
   </React.StrictMode>
 );
